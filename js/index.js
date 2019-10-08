8b4e83112ea835ea0a2ee98e179b751a5f0f74df
Stelios Vitalis <steliosvitalis@gmail.com>
2019-10-08 15:28:00 +0200

    Add file loading capabilities

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ Vue.component('version-item', {
 var app = new Vue({
     el: '#app',
     data: {
+      file_loaded: false,
       show_ids: false,
       versions: {
         "db244f6be3791d72c94b099fde8db2915c6a7041": { author: "Solid Snake", message: "Initial commit", date: new Date('2011-04-11T10:20:30Z') },
@@ -38,6 +39,35 @@ var app = new Vue({
             .mapValues('version_info')
             .value();
         }
+    },
+    methods: {
+      selectedFile() {
+        console.log("Selected a CityJSON file...");
+        console.log(this.$refs.cityJSONFile.files[0]);
+
+        let file = this.$refs.cityJSONFile.files[0];
+        if (!file || file.type != "application/json")
+        {
+          console.log("This is not a JSON file at all!!!");
+          return;
+        }
+
+        let reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload = evt => {
+          cm = JSON.parse(evt.target.result);
+
+          if ("versioning" in cm)
+          {
+            this.versions = cm["versioning"]["versions"];
+            this.file_loaded = true;
+          }
+          else
+          {
+            console.log("This is not a versioned CityJSON file!");
+          }
+        }
+      }
     }
 })
 
