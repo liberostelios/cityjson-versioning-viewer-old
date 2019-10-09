43d03dda2f395d283c7c22cbc1374dd183dd3757
Stelios Vitalis <steliosvitalis@gmail.com>
2019-10-09 17:28:52 +0200

    Add functionality to version download button

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 Vue.component('version-item', {
     props: ['version', 'vid'],
     template: `
-    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
       <div class="d-flex justify-content-between">
         <div class="col-9">
           <h5 class="mb-1">{{ version.message }}</h5>
@@ -11,13 +11,18 @@ Vue.component('version-item', {
           <div class="input-group input-group-sm justify-content-end">
             <span class="input-group-text text-monospace" id="basic-addon1"><small>{{ vid | truncate(7) }}</small></span>
             <div class="input-group-append">
-              <button class="btn btn-outline-primary" type="button"><i class="fas fa-download"></i></span></button>
+              <button class="btn btn-outline-primary" type="button" v-on:click="download_version(vid)"><i class="fas fa-download"></i></span></button>
             </div>
           </div>
         </div>
       </div>
-    </a>
-    `
+    </div>
+    `,
+    methods: {
+      download_version(vid) {
+        this.$root.$emit('download_version', vid);
+      }
+    }
 })
 
 var app = new Vue({
@@ -25,6 +30,7 @@ var app = new Vue({
     data: {
       file_loaded: false,
       active_branch: "master",
+      citymodel: {},
       versioning: {
         versions: {
           "db244f6be3791d72c94b099fde8db2915c6a7041": { author: "Solid Snake", message: "Initial commit", date: new Date('2011-04-11T10:20:30Z'), parents: [] },
@@ -35,6 +41,14 @@ var app = new Vue({
           "develop": "816590924a31e92959281353dda3ce5b3f70bf44"
         }
       }
+    },
+    created() {
+      let self = this;
+
+      this.$root.$on('download_version', (vid) => {
+        console.log(self.file_loaded);
+        self.downloadVersion(vid);
+      });
     },
     methods: {
       orderedVersions(branch) {
@@ -71,6 +85,8 @@ var app = new Vue({
         reader.onload = evt => {
           cm = JSON.parse(evt.target.result);
 
+          this.citymodel = cm;
+
           if ("versioning" in cm)
           {
             versions = cm["versioning"]["versions"];
@@ -86,6 +102,37 @@ var app = new Vue({
             console.log("This is not a versioned CityJSON file!");
           }
         }
+      },
+      download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+      
+        element.style.display = 'none';
+        document.body.appendChild(element);
+      
+        element.click();
+      
+        document.body.removeChild(element);
+      },
+      downloadVersion(vid) {
+        cityobjects = this.versioning.versions[vid].objects;
+
+        var result = $.extend({}, this.citymodel);
+
+        delete result["versioning"];
+
+        for (var key in result["CityObjects"])
+        {
+          if (cityobjects.indexOf(key) == -1)
+          {
+            delete result["CityObjects"][key];
+          }
+        }
+
+        text = JSON.stringify(result);
+
+        this.download(vid + ".json", text);
       }
     }
 })
